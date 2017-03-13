0f3d76c5029de7df96bebc2f42e3ea8c725d0b5c
Ebenezer Williams <ebenezerw@me.com>
2017-03-13 11:56:55 -0400

    can now get fullname, location, formatted image and public url from linkedin. some fields still missing

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,14 @@ class User < ApplicationRecord
             user.uid = auth.uid
             user.email = auth.info.email
             user.password = Devise.friendly_token[0, 20]
-            user.first_name = auth.info["first-name"]
+            user.first_name = auth['info']['name']
             # user.last_name = auth.lastName
-            user.headline = auth.info.headline
-            # user.location = auth.location
-            # user.position_title = auth.position
-            # user.picture_url = auth.pictureUrl
-            # user.profile_url = auth.publicProfileUrl
+            user.headline = auth['info']['headline']
+            user.location = auth['info']['location']['name']
+            # user.position_title = auth['info']['positions']['company']
+            # user.position_title = auth['info']['positions']['title']
+            user.picture_url = auth['info']['image']
+            user.profile_url = auth['info']['urls']['public_profile']
         end
     end
     has_many :users
