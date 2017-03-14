1c95d673abe2c310b4b61b3783df474fbd8fa4b4
Ebenezer Williams <ebenezerw@me.com>
2017-03-14 16:00:51 -0400

    migrate

--- db/migrate/20170313003411_add_column_to_users.rb
+++ db/migrate/20170313003411_add_column_to_users.rb
@@ -1,17 +1,17 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :provider
-    add_column :users, :uid
-    add_column :users, :first_name
-    add_column :users, :last_name
-    add_column :users, :headline
-    add_column :users, :location
-    add_column :users, :position_title
-    add_column :users, :position_company
-    add_column :users, :picture_url
-    add_column :users, :profile_url
-    add_column :users, :mentor
-    add_column :users, :user_type
-    add_column :users, :user_skills
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :headline, :string
+    add_column :users, :location, :string
+    add_column :users, :position_title, :string
+    add_column :users, :position_company, :string
+    add_column :users, :picture_url, :string
+    add_column :users, :profile_url, :string
+    add_column :users, :mentor, :boolean
+    add_column :users, :user_type, :string
+    add_column :users, :user_skills, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20170314180918) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "encrypted_password",     default: ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
