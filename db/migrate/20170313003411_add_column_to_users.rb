6b2b1bc8968bbe958a1f31ebc95c7b229532cda3
Ebenezer Williams <ebenezerw@me.com>
2017-03-14 15:48:01 -0400

    fixed migration

--- db/migrate/20170313003411_add_column_to_users.rb
+++ db/migrate/20170313003411_add_column_to_users.rb
@@ -1,17 +1,17 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :provider, :string, null: true, default: ""
-    add_column :users, :uid, :string, null: true, default: ""
-    add_column :users, :first_name, :string, null: true, default: ""
-    add_column :users, :last_name, :string, null: true, default: ""
-    add_column :users, :headline, :string, null: true, default: ""
-    add_column :users, :location, :string, null: true, default: ""
-    add_column :users, :position_title, :string, null: true, default: ""
-    add_column :users, :position_company, :string, null: true, default: ""
-    add_column :users, :picture_url, :string, null: true, default: ""
-    add_column :users, :profile_url, :string, null: true, default: ""
-    add_column :users, :mentor, :boolean, null: true, default: ""
-    add_column :users, :user_type, :string, null: true, default: ""
-    add_column :users, :user_skills, :string, null: true, default: ""
+    add_column :users, :provider
+    add_column :users, :uid
+    add_column :users, :first_name
+    add_column :users, :last_name
+    add_column :users, :headline
+    add_column :users, :location
+    add_column :users, :position_title
+    add_column :users, :position_company
+    add_column :users, :picture_url
+    add_column :users, :profile_url
+    add_column :users, :mentor
+    add_column :users, :user_type
+    add_column :users, :user_skills
   end
 end
