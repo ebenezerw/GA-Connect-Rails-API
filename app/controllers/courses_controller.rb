5d55fdd13d6417b502a50efbf7596dae231b60d0
Ebenezer Williams <ebenezerw@me.com>
2017-03-13 20:09:56 -0400

    rails api connected to angular app. can view all users, and show page per user

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,11 @@
 class CoursesController < ApplicationController
 def index
 @courses = Course.all
+render json: @courses.to_json, status: :ok
+
+end
+
+def show
 
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,22 @@ end
 
   def show
           @user = User.find(params[:id])
+          render json: @user
       end
 
+      def edit
+        @user = User.find(params[:id])
+        render json: @user
+
+           end
+
+           def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        @user.save
+        render json: @user
+
+     end
 
 
 private
